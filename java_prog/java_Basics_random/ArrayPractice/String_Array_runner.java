1161cdeb73ab8dd65a201db783f69fb2c951a5f3
Ayushi Singh <ayu.s28.ayushi@gmail.com>
2026-05-19 16:22:42 +0530

    array practice

---
+++ java_prog/java_Basics_random/ArrayPractice/String_Array_runner.java
@@ -0,0 +1,13 @@
+package java_Learning;
+
+public class String_Array_runner {
+
+	public static void main(String[] args) {
+		String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+		String_Array days = new String_Array(daysOfWeek);
+		System.out.println("Day with longest number of letters:"+ days.getLongestDay());
+		days.printDaysBackwards();
+	}
+}
+
